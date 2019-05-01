5b45c87c289333d8f28e8414e3602bc64ee80828
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-01 14:24:00 -0400

    first commit

--- db/models/Tournament.js
+++ db/models/Tournament.js
@@ -21,6 +21,11 @@ const TournamentSchema = new mongoose.Schema({
     }]
 }})
 
+
+// {"2019":
+//    [
+//     {"Team":"Duke" ,"Seed":	1	 ,"Region":"East" ,"Round1":	0.991789442	 ,"Round2":	0.908115588	 ,"Round3":	0.717264438	 ,"Round4":	0.524229344	 ,"Round5":	0.321658475	 ,"Round6":	0.193334432	},
+
 let tournament = mongoose.model("tournament", TournamentSchema)
 
 module.exports = tournament
