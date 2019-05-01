be274634930d5975b756abcf71220612fc6ff6e0
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-01 16:13:03 -0400

    update

--- db/controllers/teams.js
+++ db/controllers/teams.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router = express.Router()
 
 const Teams = require('../models/Teams')
-const ncaam2019 = require('../../node_modules/bracket-data/data/ncaam/2019')
 
 router.get("/", (req, res) => {
   console.log('received a request on /')
--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const teamsController = require('./db/controllers/teams')
 const fiveThreeController = require('./db/controllers/538tournament')
 const popPicksController = require('./db/controllers/popPicks')
 const bodyParser = require('body-parser')
-const ncaam2019 = require('./node_modules/bracket-data/data/ncaam/2019')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 
-//redirects to api/people page when people entered in URL
-app.use('/people', (req,res) => {
-	res.redirect('/api/teams/')
+//redirects to api/ncaam page when basketball entered in URL
+app.use('/basketball', (req,res) => {
+	res.redirect('/api/ncaam2019/')
 })
 
 app.use('/api/ncaam2019/', teamsController)
