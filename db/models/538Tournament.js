1d1c6c9342dcc53f0e0454ee7bbbeced4a47203b
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-07 09:37:20 -0400

    re-ordered pop picks

--- db/models/538Tournament.js
+++ db/models/538Tournament.js
@@ -3,27 +3,20 @@ const mongoose = require('../connection')
 const Schema= mongoose.Schema
 
 const FiveThreeEightSchema = new mongoose.Schema({
-  Year: Number,
-  Bracket: [ 
-    {Region: String, Schools: [ 
-      {
-        Team: {type: Schema.Types.String, ref: "teams" },
-        Seed: String,
-        Round1: Number,
-        Round2: Number,
-        Round3: Number,
-        Round4: Number,
-        Round5: Number,
-        Round6: Number
-    }
-  ]}
-      
+  Year: Number, Bracket: [ 
+    {
+      Team: {type: Schema.Types.String, ref: "teams" },
+      Seed: String,
+      Region: String,
+      Round1: Number,
+      Round2: Number,
+      Round3: Number,
+      Round4: Number,
+      Round5: Number,
+      Round6: Number
+  }]
 
-    ]
-
-  }
-
-)
+})
 
 let tournament = mongoose.model("tournament", FiveThreeEightSchema)
 
