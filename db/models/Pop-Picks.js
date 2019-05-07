a89f54f7903547bf73d91e026c1f24a6906e2c99
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-07 09:45:59 -0400

    fixed schemas

--- db/models/Pop-Picks.js
+++ db/models/Pop-Picks.js
@@ -3,20 +3,27 @@ const mongoose = require('../connection')
 const Schema= mongoose.Schema
 
 const popPicksSchema = new mongoose.Schema({
-  Year: Number, Bracket: [ 
+  Year: Number,
+  Bracket: [ 
+    {Region: String, Schools: [ 
       {
         Team: {type: Schema.Types.String, ref: "teams" },
         Seed: String,
-        Region: String,
         Round1: Number,
         Round2: Number,
         Round3: Number,
         Round4: Number,
         Round5: Number,
         Round6: Number
-    }]
+    }
+  ]}
+      
 
-})
+    ]
+
+  }
+
+)
 
 let ppicks = mongoose.model("ppicks", popPicksSchema)
 
