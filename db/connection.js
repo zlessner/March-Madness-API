ca6ce0b68862d6a41205c4e4926f003d479dea5e
zlessner <LessnerZachary@gmail.com>
2021-06-29 16:18:54 -0400

    most updated 2

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+// const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV == "production") {
+	mongoose.connect(process.env.DB_URL)
+  } else {
+	mongoose.connect("mongodb://localhost/Project2")
+	// mongoose.connect("mongodb://localhost/Project2") 
+  }
+
+mongoose.Promise = Promise
+
+mongoose.connect('mongodb://localhost/Project2', { useNewUrlParser: true })
+.then((conn) => {
+	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
+})
+.catch(err => {
+	console.error(err)
+})
+
+
+module.exports = mongoose
+
